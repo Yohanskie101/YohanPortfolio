b70882641a9075b6e6a33adafafc4e42a07d7c56
Yohanskie101 <125958519+Yohanskie101@users.noreply.github.com>
2024-07-09 19:04:25 +0800

    New scroll animation for menubar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // Intersection Observer setup
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
+
+  // Smooth scrolling setup
+  document.querySelectorAll(".top-list button").forEach((button) => {
+    button.addEventListener("click", function () {
+      const targetId = this.getAttribute("data-target");
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
   });
 });
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
